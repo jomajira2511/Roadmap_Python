b46cccfcda8a08f85806c07ade5dd870c1a2d082
Jomajira <jomajira2511@gmail.com>
2025-08-05 22:48:59 -0500

    Se agrega la primer parte de la tercera seccion del curso (cadenas de caracteres)

---
+++ 1. Python Practicando Desde 0 hasta desarrollador python/03. Cadenas De Texto/01.Cadenas_texto.py
@@ -0,0 +1,28 @@
+"""
+===================================================
+Script: 01.Cadenar_texto.py
+Proyecto: Roadmap Python
+Autor: Jhon Jiménez
+GitHub: https://github.com/jomajira2511
+Correo: jomajira2511@gmail.com
+Fecha de creación: 2025-08-05
+Descripción:
+    Este script muestra ejemplos del uso de cadenas de texto
+    o string, el manejo basico y algunas funcionalidades 
+===================================================
+"""
+
+Cadena = "Ejemplo de cadena de texto "  ##String o cadena de texto
+Cadena_con_salto= "\nSalto de linea usando \nSigo en la otra linea " ##usando \n se pueden hacer saltos de linea 
+print(Cadena)
+print(Cadena_con_salto)
+print ("\n-----------------------------------\n")
+
+Nombre = "Jhon"
+Apellido = "Jimenez"
+print ("Nombre :",Nombre)
+print ("Apellido :",Nombre)
+print ("Usando \"+\" como conector de cadenas tenemos :", Nombre + Apellido)  ##usando + se pueden concatenar cadenas 
+print("Usando \"*\"  permite multiplicar las cadenas  :", Nombre * 3)
+
+
