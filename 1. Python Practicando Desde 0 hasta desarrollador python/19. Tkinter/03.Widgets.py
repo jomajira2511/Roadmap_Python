1bcaed2b7cb3ede5e89b13a2fc3f99e26b237fe4
Jomajira <jomajira2511@gmail.com>
2025-09-01 00:34:09 -0500

    Se modifica la funcionalidad del boton y el label, y se da un style basico a las etiquetas dando los primeros pasos en el style usando tkinter

--- 1. Python Practicando Desde 0 hasta desarrollador python/19. Tkinter/03.Widgets.py
+++ 1. Python Practicando Desde 0 hasta desarrollador python/19. Tkinter/03.Widgets.py
@@ -5,27 +5,34 @@ ventana = tk.Tk()
 ventana.title("Widgets")
 ventana.geometry("700x700")
 
+#Espacio para definir funciones 
+
+def AbrirGoogle():  #Permite al momento de presionar un boton, nos lleve automaticamente a google 
+    webbrowser.open("https://www.google.com")
+
 # Etiqueta o label en tkinter
 etiqueta = tk.Label(
     ventana, text="Este es mi primer label", 
     bg="red",
     fg="white", 
-    font="30",
+    font= ("Helvetica",30,"italic"),
     width="30",
     height="5"
     )
-etiqueta.pack()
+etiqueta.pack(pady = 30)
 
 # Botón en tkinter para abrir Google
 boton = tk.Button(
     ventana, 
     text="Presiona este botón para abrir Google",  # Permite colocarle texto al boton
-    command=lambda: webbrowser.open("https://www.google.com")  #Command, permite darle una accion una vez se presione el texto 
+    command= AbrirGoogle, #Command, permite darle una accion una vez se presione el texto 
+    bg="lightgreen", # Colocar un fondo de color al botn
+    font=("Courier", 20,"bold"), # Cambiar el dise;o 
+    border=10, # Asignar el borde
+    activebackground="lightpink",
+    activeforeground="gold"
+
 )
 boton.pack()
-
-#Pendiente agregar correciones
-boton.tk_setPalette()
-
 
 ventana.mainloop()
