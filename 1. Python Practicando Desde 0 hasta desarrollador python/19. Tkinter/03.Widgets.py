99e6919222a5f3b03e0ea57a25653680fe0812dd
Jomajira <jomajira2511@gmail.com>
2025-08-31 23:55:36 -0500

    Primera version de archivo widgets usando tkinter

---
+++ 1. Python Practicando Desde 0 hasta desarrollador python/19. Tkinter/03.Widgets.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import webbrowser 
+
+ventana = tk.Tk()
+ventana.title("Widgets")
+ventana.geometry("700x700")
+
+# Etiqueta o label en tkinter
+etiqueta = tk.Label(
+    ventana, text="Este es mi primer label", 
+    bg="red",
+    fg="white", 
+    font="30",
+    width="30",
+    height="5"
+    )
+etiqueta.pack()
+
+# Botón en tkinter para abrir Google
+boton = tk.Button(
+    ventana, 
+    text="Presiona este botón para abrir Google",  # Permite colocarle texto al boton
+    command=lambda: webbrowser.open("https://www.google.com")  #Command, permite darle una accion una vez se presione el texto 
+)
+boton.pack()
+
+
+ventana.mainloop()
