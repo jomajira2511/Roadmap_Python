525eb6084999adfc0e89cc737f9c464e4399087a
Jomajira <jomajira2511@gmail.com>
2025-09-08 16:14:12 -0500

    Finaliza el directorio Dia 1 operadores

---
+++ 2. Python en 30 dias/01. Dia 1 Operadores/04.Operadores_asignacion.py
@@ -0,0 +1,50 @@
+"""
+===================================================
+Script: 04.Operadores_asignacion.py
+Proyecto: Roadmap Python
+Autor: Jhon Jiménez
+GitHub: https://github.com/jomajira2511
+Correo: jomajira2511@gmail.com
+Fecha de creación: 2025-09-08
+Descripción:
+    Ejemplo práctico de operadores de asignación en Python.
+    Se muestran operaciones matemáticas básicas como:
+    suma, resta, módulo, división y potencia, aplicadas
+    directamente a variables.
+===================================================
+"""
+
+# -------------------------------
+# Operador de suma
+# Asigna el resultado de la suma (10 + 20) a la variable 'a'
+# -------------------------------
+a = 10 + 20
+print("Resultado de la suma:", a)
+
+# -------------------------------
+# Operador de resta
+# Asigna el resultado de la resta (10 - 20) a la variable 'b'
+# -------------------------------
+b = 10 - 20
+print("Resultado de la resta:", b)
+
+# -------------------------------
+# Operador módulo (%)
+# Devuelve el residuo de la división (10 % 5)
+# -------------------------------
+c = 10 % 5
+print("Resultado del módulo:", c)
+
+# -------------------------------
+# Operador de división (/)
+# Asigna el resultado de dividir (10 / 2) a la variable 'd'
+# -------------------------------
+d = 10 / 2
+print("Resultado de la división:", d)
+
+# -------------------------------
+# Operador de potencia (**)
+# Calcula 10 elevado a la 2
+# -------------------------------
+e = 10 ** 2
+print("Resultado de la potencia:", e)
