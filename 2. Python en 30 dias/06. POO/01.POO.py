d6ac95554edbbe89df268e4d8b4cdf2c973a0f89
Jomajira <jomajira2511@gmail.com>
2025-09-11 21:37:16 -0500

    Se agrega nuevo archivo al directorio sobre POO

--- 2. Python en 30 dias/06. POO/01.POO.py
+++ 2. Python en 30 dias/06. POO/01.POO.py
@@ -1,24 +1,55 @@
-#Programacion orientada a aobjetos o POO
+"""
+===================================================
+Script: 01.POO.py
+Proyecto: Roadmap Python
+Autor: Jhon Jiménez
+GitHub: https://github.com/jomajira2511
+Correo: jomajira2511@gmail.com
+Fecha de creación: 2025-09-11
+Descripción:
+    Ejemplo básico de Programación Orientada a Objetos en Python.
+    - Clase Gelatina: con atributos sabor, color y tamaño.
+    - Clase Persona: con atributos nombre, edad, nacionalidad y sexo.
+    - Métodos para imprimir la información de cada objeto.
+===================================================
+"""
 
-class Gelatina():  ##estructura de una clase 
-    def __init__(self, sabor, color, tamanio):   # creamos un contructor, qu eincializa las caracteristicas de la calse
-        self.sabor = sabor #Estas son las caracterisitcas o el molde, tambine conocido como contructor
+# Primera clase
+class Gelatina():
+    # Constructor
+    def __init__(self, sabor, color, tamanio):
+        self.sabor = sabor
         self.color = color
         self.tamanio = tamanio 
     
     def imprimir(self):
         print(f"La gelatina es de {self.sabor}")
         print(f"La gelatina se ve {self.color}")
-        print(f"La gelatina es de un tamanio {self.tamanio}")
+        print(f"La gelatina es de un tamaño {self.tamanio}\n")
 
 
-gel1  = Gelatina("Fresa", "Rojo","Grande")  #Creamos una instancia de la clase 
-gel2 = Gelatina("Mora", "morado" , "Mediana")
+# Segunda clase
+class Persona():
+    def __init__(self, nombre, nacionalidad, edad, sexo):
+        self.nombre = nombre
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+        self.sexo = sexo 
 
-gel1.imprimir()   #llamamos la clase 
+    def imprimirPersona(self):
+        print(f"Tu nombre es: {self.nombre}")
+        print(f"Tu edad es: {self.edad}")
+        print(f"Tu sexo es: {self.sexo}")
+        print(f"Tu nacionalidad es: {self.nacionalidad}\n")
+
+
+# Creamos instancias de Gelatina
+gel1  = Gelatina("Fresa", "Rojo", "Grande")
+gel2 = Gelatina("Mora", "Morado", "Mediana")
+
+gel1.imprimir()
 gel2.imprimir()
 
-
-
-
-
+# Creamos instancia de Persona
+persona1 = Persona("Jhon", "Colombiano", 25, "Masculino")
+persona1.imprimirPersona()
