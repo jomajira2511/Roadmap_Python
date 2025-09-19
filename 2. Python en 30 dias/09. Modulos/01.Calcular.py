dff38865383a6a0456aa852397e45541d7c02a10
Jomajira <jomajira2511@gmail.com>
2025-09-19 15:40:57 -0500

    Se crea nuevo directorio sobre modulos

---
+++ 2. Python en 30 dias/09. Modulos/01.Calcular.py
@@ -0,0 +1,37 @@
+"""
+===================================================
+Script: 01.Calcular.py
+Proyecto: Roadmap Python
+Autor: Jhon Jiménez
+GitHub: https://github.com/jomajira2511
+Correo: jomajira2511@gmail.com
+Fecha de creación: 2025-09-19
+Descripción:
+    Script que importa el módulo `Calculadora.py` 
+    y utiliza sus funciones para realizar operaciones 
+    matemáticas básicas como suma y resta.
+===================================================
+"""
+
+# ===================================================
+# Importamos el módulo Calculadora
+# El asterisco (*) importa todas las funciones del archivo Calculadora.py
+# Esto permite llamar directamente a las funciones sin prefijo de módulo.
+# ===================================================
+from Calculadora import *
+
+# ===================================================
+# Ejemplo de uso de las funciones de la calculadora
+# ===================================================
+
+# Suma de dos números
+sumar(3, 3)
+
+# Resta de dos números
+restar(3, 1)
+
+# Division de dos numero 
+dividir(2,2)
+
+# Multiplicacion de dos numeros
+multiplicacion(3,3)
